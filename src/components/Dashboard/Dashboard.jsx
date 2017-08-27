1879a182fb9eb0606f33f05f37c15f3dcf1e4ee0
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-08-27 04:27:05 -0400

    Configured searchbar for dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import SearchBar from '../Header/SearchBar';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../../actions/header';
+import {push} from 'react-router-redux';
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
 	constructor() {
 		super();
 	}
-	
+
+	render() {
+		<li className="dashboard">
+			<ul>
+				<SearchBar inputValue={this.props.inputValue}
+						onChange={this.props.onChange}
+						onSearch={this.props.onSearch}
+						data={this.props.data}
+						history={this.props.history} 
+						location={this.props.location}
+						goto = {this.props.goto}/>
+			</ul>
+		</li>
+	}
 }
+
+function mapStatetoProps(state) {
+	return {
+		inputValue: state.app.inputValue,
+		data: state.app.data
+	}
+}
+
+function mapDispatchtoProps(dispatch) {
+	return {
+		onChange: bindActionCreators(actions.onChange, dispatch),
+		onSearch: bindActionCreators(actions.onSearch, dispatch),
+		goto: function(path) { dispatch( push(path) ) }
+	}
+}
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(Header)
